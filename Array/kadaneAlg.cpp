b2e1d381b4f7e2e9b79c179a6519545ac803087e
Tejoyasaswi-17 <saisankar1702@gmail.com>
2021-11-23 17:18:41 +0530

    Modified kadane's algorithms by adding some of its use cases in other problems

--- Array/kadaneAlg.cpp
+++ Array/kadaneAlg.cpp
@@ -5,6 +5,14 @@
 #include <climits>
 #include <algorithm>
 using namespace std;
+/*
+Contents of the file
+1. Brute force for Maximum subarray sum
+2. Cumulative sum approach
+3. Kadane's Algorithm
+4. Maximum circular subarray sum
+5. Pair sum
+*/
 
 // Find the subarray in an array which has maximum sum
 // This sum can be solved in multiple ways of which the best is to solve using kadane's algorithm which has O() time complexity
@@ -81,6 +89,7 @@ int main()
 }
 */
 // Second version, Simple version
+/*
 int main()
 {
     int arr[]{-1,4,-3,7,-4};
@@ -96,4 +105,49 @@ int main()
         }
     }
     cout << maxSum << endl;
+}
+*/
+// 4. Maximum circular subarray sum
+
+int kadane(int arr[], int n)
+{
+    int maxSum = INT_MIN, sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum+=arr[i];
+        maxSum = max(maxSum, sum);
+        if(sum<0)
+        {
+            sum = 0;
+        }
+    }
+    return maxSum;
+}
+
+int maxCircularSum(int arr[], int n)
+{
+    int arrSum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        arrSum+=arr[i];
+    }
+    int tempArr[n];
+    for(int i = 0; i < n; i++)
+    {
+        tempArr[i] = -1 * arr[i];
+    }
+    int nonContribSum = kadane(tempArr,n);
+    int wrapSum = arrSum+nonContribSum;
+    int nonWrapSum = kadane(arr,n);
+    if(nonWrapSum<0)
+    {
+        return nonWrapSum;
+    }
+    return max(nonWrapSum, wrapSum);
+}
+int main()
+{
+    int a[] = {-2,-3,-1};//{ 11, 10, -20, 5, -3, -5, 8, -13, 10 };
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << maxCircularSum(a, n) << endl;
 }
